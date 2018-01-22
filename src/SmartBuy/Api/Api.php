d3b2ecc99d0a8b65c1435e4e67fd0b6a1b1dfe58
Maris <marislazda@gmail.com>
2018-01-22 20:48:31 +0200

    Header update

--- src/SmartBuy/Api/Api.php
+++ src/SmartBuy/Api/Api.php
@@ -56,6 +56,9 @@ class Api
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Accept: application/json'
+        ]);
 
         $return = curl_exec($ch);
         curl_close ($ch);
