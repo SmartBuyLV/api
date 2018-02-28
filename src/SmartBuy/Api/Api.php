f9626ac0e35691492951edeec00d46960ce5ee34
Maris <marislazda@gmail.com>
2018-02-28 19:41:35 +0200

    API updates (products []) + cancel/delete orders

--- src/SmartBuy/Api/Api.php
+++ src/SmartBuy/Api/Api.php
@@ -10,7 +10,8 @@ class Api
     private $cookieLifetime = 0;
     private $cookieName = 'affiliate-smartbuy';
     private $accessKey;
-    private $host = 'https://www.smartbuy.lv/api/v1';
+    //private $host = 'https://www.smartbuy.lv/api/v1';
+    private $host = 'http://cashback.hmchoose.com/api/v1';
 
     function __construct($accessKey = null, $cookieLifetime = 0, $cookieName = null)
     {
@@ -30,14 +31,47 @@ class Api
         }
     }
 
-    public function registerOrder($referenceNumber, $amount, $rate = null)
+    public function orderRegister($referenceNumber, $products = [], $referralId = null)
     {
-        if (empty($this->getReferral())) {
+        $referralId = ($referralId ? $referralId : $this->getReferral());
+        $params = [
+            'referralId' => $referralId,
+            'referenceNumber' => $referenceNumber,
+            'products' => $products
+        ];
+
+        return $this->call('orders', 'PUT', $params);
+    }
+
+    public function orderCancel($referenceNumber, $reason = null)
+    {
+        $params = [
+            'referenceNumber' => $referenceNumber,
+            'reason' => $reason
+        ];
+
+        return $this->call('orders', 'PATCH', $params);
+    }
+
+    public function orderDelete($referenceNumber)
+    {
+        $params = [
+            'referenceNumber' => $referenceNumber,
+        ];
+
+        return $this->call('orders', 'DELETE', $params);
+    }
+
+    public function registerOrder($referenceNumber, $amount, $rate = null, $referralId = null)
+    {
+        $referralId = ($referralId ? $referralId : $this->getReferral());
+
+        if (empty($referralId)) {
             return null;
         }
 
         $params = [
-            'referralId' => $this->getReferral(),
+            'referralId' => $referralId,
             'referenceNumber' => $referenceNumber,
             'amount' => $amount,
             'rate' => $rate
@@ -46,9 +80,30 @@ class Api
         return $this->post('order', $params);
     }
 
-    private function getReferral()
+    public function getReferral()
     {
         return (!empty($_COOKIE[$this->cookieName]) ? (int)$_COOKIE[$this->cookieName] : 0);
+    }
+
+    private function call($method, $request, $params = [])
+    {
+
+        $params['accessToken'] = $this->accessKey;
+        $data = json_encode($params);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $this->host . '/' . $method);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $request);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Accept: application/json',
+            'Content-type: application/json'
+        ]);
+
+        $return = curl_exec($ch);
+        curl_close ($ch);
+
+        return $return;
     }
 
     private function post($method, $params = [])
